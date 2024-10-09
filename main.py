776666e71f0d66798edd5772cc79bfc42c088802
Fahad <fahad.alabdulkarim5@gmail.com>
2024-10-09 13:12:35 +0300

    LAB_FILES_IO

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+def to_do_list():
+    while True:
+        answer = input("do you want to add a new To-Do item? ")
+        if answer.upper() == "Y":
+            to_do= input("type your new to do item: ")
+            file = open("to_do.txt", "a", encoding="UTF-8")
+            file.write(to_do + "\n")
+            file.close()
+        elif answer.upper() == "N":
+            ans = input("do you want to list your To-Do items? ")
+            if ans.upper() == "Y":
+                file = open("to_do.txt", "r", encoding="UTF-8")
+                file.seek(0)
+                print(file.read())
+                file.close()
+            elif ans.upper() == "N":
+                to_do_list()
+            elif ans.upper() == "EXIT":
+                break
+        elif answer.upper() == "EXIT":
+            break
+        else:
+            print("enter a valid answer (y or n)")
+            to_do_list()
+
+to_do_list()
+    
